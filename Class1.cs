1fe4c00e1d3bcfb878de4c92c86c292d1434ad08
离披 <lipi@shuise.dev>
2026-02-24 18:06:47 +0800

    适配breeders

--- Class1.cs
+++ Class1.cs
@@ -11,18 +11,17 @@ namespace OldScenarioAnalyzer
         [PluginDescription("解析旧剧本(种田杯以前)回合信息")]
         public string Name => "OldScenarioAnalyzer";
         public string Author => "UmamusumeResponseAnalyzer";
-        public Version Version => new(1, 0, 0);
         public string[] Targets => [];
         public async Task UpdatePlugin(ProgressContext ctx)
         {
-            var progress = ctx.AddTask($"[{Name}] 更新");
+            var progress = ctx.AddTask($"[[{Name}]] 更新");
 
             using var client = new HttpClient();
             using var resp = await client.GetAsync($"https://api.github.com/repos/URA-Plugins/{Name}/releases/latest");
             var json = await resp.Content.ReadAsStringAsync();
             var jo = JObject.Parse(json);
 
-            var isLatest = ("v" + Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
+            var isLatest = ("v" + ((IPlugin)this).Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
             if (isLatest)
             {
                 progress.Increment(progress.MaxValue);
